32a0405247fe21e5a3c378004012e91cf3cfa80d
Shawn Brown <shawnbrown@users.noreply.github.com>
2020-03-17 21:38:00 -0400

    Add an else condition to set velocity to zero when there is no input and reduce the velocity to slow the player movement.

--- Player/Player.gd
+++ Player/Player.gd
@@ -4,8 +4,10 @@ var velocity = Vector2.ZERO
 
 func _physics_process(delta):
 	if Input.is_action_pressed("ui_right"):
-		velocity.x = 10
-	if Input.is_action_pressed("ui_left"):
-		velocity.x = -10
+		velocity.x = 4
+	elif Input.is_action_pressed("ui_left"):
+		velocity.x = -4
+	else:
+		velocity.x = 0
 
 	move_and_collide(velocity)
