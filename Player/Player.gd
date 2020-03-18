3c3a6cf4308e459d93db7247dfaf6392c5b76872
Shawn Brown <shawnbrown@users.noreply.github.com>
2020-03-17 21:53:53 -0400

    Replace is_action_pressed() with get_action_strength().
    This arrangement gives smoother movement and allows proper
    composition of diagonal input.

--- Player/Player.gd
+++ Player/Player.gd
@@ -3,16 +3,14 @@ extends KinematicBody2D
 var velocity = Vector2.ZERO
 
 func _physics_process(delta):
-	if Input.is_action_pressed("ui_right"):
-		velocity.x = 4
-	elif Input.is_action_pressed("ui_left"):
-		velocity.x = -4
-	elif Input.is_action_pressed("ui_down"):
-		velocity.y = 4
-	elif Input.is_action_pressed("ui_up"):
-		velocity.y = -4
+	var input_vector = Vector2.ZERO
+
+	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
+	input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
+
+	if input_vector != Vector2.ZERO:  # <- This if/else is unnecessary right now
+		velocity = input_vector       #    but will be useful when we add friction.
 	else:
-		velocity.x = 0
-		velocity.y = 0
+		velocity = Vector2.ZERO
 
 	move_and_collide(velocity)
