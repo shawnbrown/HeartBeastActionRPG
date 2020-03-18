65da518d52ef73eb64bd85dce19c0dc81ded2607
Shawn Brown <shawnbrown@users.noreply.github.com>
2020-03-18 01:51:40 -0400

    Tighten up _physics_process() function.
    Move variable declaration out of function and minimize input vector handling.

--- Player/Player.gd
+++ Player/Player.gd
@@ -5,16 +5,15 @@ const MAX_SPEED = 100
 const FRICTION = 10
 
 var velocity = Vector2.ZERO
+var x = 0.0
+var y = 0.0
 
 func _physics_process(delta):
-	var input_vector = Vector2(
-		Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left"),
-		Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
-	)
+	x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
+	y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
 
-	if input_vector != Vector2.ZERO:
-		input_vector = input_vector.normalized()  # Scale to unit length.
-		velocity = velocity.move_toward(input_vector * MAX_SPEED, ACCELERATION)
+	if x or y:
+		velocity = velocity.move_toward(Vector2(x, y).normalized() * MAX_SPEED, ACCELERATION)
 	else:
 		velocity = velocity.move_toward(Vector2.ZERO, FRICTION)
 
