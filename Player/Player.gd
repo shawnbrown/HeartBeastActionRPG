dd2449d4d2d027644a5c80c724496f1464ffbbbe
Shawn Brown <shawnbrown@users.noreply.github.com>
2020-03-18 01:29:44 -0400

    Move normalized vector inside if-condition so it's only calculated if needed.

--- Player/Player.gd
+++ Player/Player.gd
@@ -11,9 +11,9 @@ func _physics_process(delta):
 		Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left"),
 		Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
 	)
-	input_vector = input_vector.normalized()  # Scale to unit length.
 
 	if input_vector != Vector2.ZERO:
+		input_vector = input_vector.normalized()  # Scale to unit length.
 		velocity = velocity.move_toward(input_vector * MAX_SPEED, ACCELERATION)
 	else:
 		velocity = velocity.move_toward(Vector2.ZERO, FRICTION)
