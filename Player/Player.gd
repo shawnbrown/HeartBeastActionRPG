d5c38f51c05d0c5df604a4720d6028d3334603ea
Shawn Brown <shawnbrown@users.noreply.github.com>
2020-03-17 23:17:02 -0400

    Scale vector with normalized() to fix diagonal movement speed.

--- Player/Player.gd
+++ Player/Player.gd
@@ -9,6 +9,7 @@ func _physics_process(delta):
 
 	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
 	input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
+	input_vector = input_vector.normalized()  # Scale to unit length.
 
 	if input_vector != Vector2.ZERO:
 		velocity = input_vector
