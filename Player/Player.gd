3bc7f9bf6da93d3608640b9adfff9892059693fe
Shawn Brown <shawnbrown@users.noreply.github.com>
2020-03-17 23:02:45 -0400

    Use time "delta" to move the Player per-second instead of per-frame.

--- Player/Player.gd
+++ Player/Player.gd
@@ -1,5 +1,7 @@
 extends KinematicBody2D
 
+const MAX_SPEED = 100
+
 var velocity = Vector2.ZERO
 
 func _physics_process(delta):
@@ -8,9 +10,9 @@ func _physics_process(delta):
 	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
 	input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
 
-	if input_vector != Vector2.ZERO:  # <- This if/else is unnecessary right now
-		velocity = input_vector       #    but will be useful when we add friction.
+	if input_vector != Vector2.ZERO:
+		velocity = input_vector
 	else:
 		velocity = Vector2.ZERO
 
-	move_and_collide(velocity)
+	move_and_collide(velocity * delta * MAX_SPEED)
