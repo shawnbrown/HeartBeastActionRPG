b0baf11faea36f5c760c0ba0521a10ed29ad2dcb
Shawn Brown <shawnbrown@users.noreply.github.com>
2020-03-20 22:11:13 -0400

    Play RunRight and IdleRight based on Player movement (for testing).

--- Player/Player.gd
+++ Player/Player.gd
@@ -15,8 +15,10 @@ func _physics_process(delta):
 	y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
 
 	if x or y:
+		animation_player.play("RunRight")
 		velocity = velocity.move_toward(Vector2(x, y).normalized() * MAX_SPEED, ACCELERATION)
 	else:
+		animation_player.play("IdleRight")
 		velocity = velocity.move_toward(Vector2.ZERO, FRICTION)
 
 	velocity = move_and_slide(velocity)
