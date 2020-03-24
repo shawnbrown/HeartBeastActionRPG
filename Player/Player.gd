527bfd63ea09941e665656b4bd17b37536cc6256
Shawn Brown <shawnbrown@users.noreply.github.com>
2020-03-23 23:37:28 -0400

    Replace separate x and y variables with input_vector variable.
    To interact with the AnimationTree, we need to provide the
    input vector a few times and we want to reuse a single instance
    not create multiple instances.

--- Player/Player.gd
+++ Player/Player.gd
@@ -5,21 +5,20 @@ const MAX_SPEED = 100
 const FRICTION = 10
 
 var velocity = Vector2()
-var x = 0.0
-var y = 0.0
+var input_vector = Vector2()
 
 onready var animation_player = get_node("AnimationPlayer")
 
 func _physics_process(_delta):
-	x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
-	y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
+	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
+	input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
 
-	if x or y:
-		if x > 0:
+	if input_vector != Vector2.ZERO:
+		if input_vector.x > 0:
 			animation_player.play("RunRight")
 		else:
 			animation_player.play("RunLeft")
-		velocity = velocity.move_toward(Vector2(x, y).normalized() * MAX_SPEED, ACCELERATION)
+		velocity = velocity.move_toward(input_vector.normalized() * MAX_SPEED, ACCELERATION)
 	else:
 		animation_player.play("IdleRight")
 		velocity = velocity.move_toward(Vector2.ZERO, FRICTION)
