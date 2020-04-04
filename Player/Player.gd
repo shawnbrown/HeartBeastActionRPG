3af468ec25692d9bf8c82ebf39d9b4eaf3f4acbb
Shawn Brown <shawnbrown@users.noreply.github.com>
2020-04-04 01:32:50 -0400

    Move input and velocity handling into a separate move_state() function.

--- Player/Player.gd
+++ Player/Player.gd
@@ -15,6 +15,9 @@ func _ready():
 	animation_tree.active = true
 
 func _physics_process(_delta):
+	move_state()
+
+func move_state():
 	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
 	input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
 
