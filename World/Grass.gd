f0270c93cc2345acbc7ed1b9f4597faca61b2891
Shawn Brown <shawnbrown@users.noreply.github.com>
2020-04-11 02:38:49 -0400

    Change Grass script to trigger grass effect when signal is received.

--- World/Grass.gd
+++ World/Grass.gd
@@ -1,13 +1,12 @@
 extends Node2D
 
-func _process(delta):
-	if Input.is_action_just_pressed("attack"):
-		var GrassEffect = load("res://Effects/GrassEffect.tscn")
-		var grass_effect = GrassEffect.instance()
-		var world = get_tree().current_scene
-		world.add_child(grass_effect)
-		grass_effect.global_position = self.global_position
-		queue_free()
+func create_grass_effect():
+	var GrassEffect = load("res://Effects/GrassEffect.tscn")
+	var grass_effect = GrassEffect.instance()
+	var world = get_tree().current_scene
+	world.add_child(grass_effect)
+	grass_effect.global_position = self.global_position
 
 func _on_Hurtbox_area_entered(area):
-	pass # Replace with function body.
+	create_grass_effect()
+	queue_free()
