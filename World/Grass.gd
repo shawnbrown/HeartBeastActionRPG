3706aca81b3b6c1aa42bb1db4ad28e1f1d2d34d5
Shawn Brown <shawnbrown@users.noreply.github.com>
2020-04-10 23:31:07 -0400

    On attack action, instantiate GrassEffect scene before removing grass.

--- World/Grass.gd
+++ World/Grass.gd
@@ -2,4 +2,8 @@ extends Node2D
 
 func _process(delta):
 	if Input.is_action_just_pressed("attack"):
+		var GrassEffect = load("res://Effects/GrassEffect.tscn")
+		var grass_effect = GrassEffect.instance()
+		var world = get_tree().current_scene
+		world.add_child(grass_effect)
 		queue_free()
