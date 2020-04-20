d5683f64d293241c15350c06afdddd8a616f9da5
Shawn Brown <shawnbrown@users.noreply.github.com>
2020-04-20 01:04:27 -0400

    Change hurtbox function to subtract stats.health when called.

--- Enemies/Bat.gd
+++ Enemies/Bat.gd
@@ -9,4 +9,6 @@ func _physics_process(delta):
 	knockback = move_and_slide(knockback)
 
 func _on_Hurtbox_area_entered(area):
+	stats.health -= 1
+	print(stats.health)
 	knockback = area.knockback_vector * 120
