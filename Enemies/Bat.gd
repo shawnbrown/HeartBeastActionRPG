b9bae2b1ed7c13a6f733432061f6a8bfe40f69ae
Shawn Brown <shawnbrown@users.noreply.github.com>
2020-04-20 00:46:08 -0400

    Add reference to stats instance and set Max Health to 3 (using GUI).

--- Enemies/Bat.gd
+++ Enemies/Bat.gd
@@ -2,6 +2,8 @@ extends KinematicBody2D
 
 var knockback = Vector2()
 
+onready var stats = get_node("Stats")
+
 func _physics_process(delta):
 	knockback = knockback.move_toward(Vector2.ZERO, 200 * delta)
 	knockback = move_and_slide(knockback)
