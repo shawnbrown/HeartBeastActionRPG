3f1f3868e9ef0f1ff726b03f3d789ed97799f42f
Shawn Brown <shawnbrown@users.noreply.github.com>
2020-04-20 01:00:33 -0400

    Change Stats.health to use "onready" to get instance value (instead of the default class value).

--- Enemies/Bat.gd
+++ Enemies/Bat.gd
@@ -4,10 +4,6 @@ var knockback = Vector2()
 
 onready var stats = get_node("Stats")
 
-func _ready():
-	print(stats.max_health)
-	print(stats.health)
-
 func _physics_process(delta):
 	knockback = knockback.move_toward(Vector2.ZERO, 200 * delta)
 	knockback = move_and_slide(knockback)
--- Stats.gd
+++ Stats.gd
@@ -1,4 +1,4 @@
 extends Node
 
 export(int) var max_health = 1
-var health = max_health
+onready var health = max_health  # Use onready to get value of instance.
