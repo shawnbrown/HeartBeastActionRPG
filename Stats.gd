a85be01de72183ab8f4c481919aa9404736a71a1
Shawn Brown <shawnbrown@users.noreply.github.com>
2020-04-24 02:40:52 -0400

    Define setter function for Stats.health attribute.

--- Stats.gd
+++ Stats.gd
@@ -1,6 +1,10 @@
 extends Node
 
 export(int) var max_health = 1
-onready var health = max_health  # Use onready to get value of instance.
+onready var health = max_health  setget set_health
 
 signal no_health
+
+func set_health(value):
+	health = value
+	print("health set to " + String(health))
